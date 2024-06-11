9c770035857c42736f9a1533700aaf306a62d23a
XGNoir95 <shuhridabrar2016@gmail.com>
2024-06-11 19:08:53 +0600

    Frontend work

--- gameNova-client/src/App.jsx
+++ gameNova-client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>Welcome to GameNova!</div>
+      <nav>Site Logo</nav>
+      <Outlet/>
     </>
   )
 }
---
+++ gameNova-client/src/components/SingleBook.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const SingleBook = () => {
+    return (
+        <div>SingleBook</div>
+    )
+}
+
+export default SingleBook
