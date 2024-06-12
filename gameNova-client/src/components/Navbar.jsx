43a7408feda567932d3235bca5039ecbb2104762
XGNoir95 <shuhridabrar2016@gmail.com>
2024-06-12 21:44:38 +0600

    Adding banner

--- gameNova-client/src/components/Navbar.jsx
+++ gameNova-client/src/components/Navbar.jsx
@@ -39,36 +39,35 @@ const Navbar = () => {
 
     return (
         <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
-            <nav className={`flex items-center justify-between p-4 ${isSticky ? 'sticky top-0 bg-white shadow-md' : ''}`}>
-                <div>
+            <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
+                <div className='flex justify-between items-center text-base gap-8'>
                     {/* logo */}
                     <Link to="/" className='text-2xl font-bold text-blue-700 flex items-center gap-2'>
                         <img src={logo} alt="GameNova Logo" style={{ width: '180px', height: '20px' }} />
                     </Link>
-                </div>
-                {/* nav items for large devices */}
-                <ul className='md:flex space-x-12 hidden'>
-                    {
-                        navItems.map(({ link, path }) => (
-                            <li key={path}>
-                                <Link to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>
+                
+                    {/* nav items for large devices */}
+                    <ul className='md:flex space-x-12 hidden'>
+                        {
+                            navItems.map(({ link, path }) => (
+                                <Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>
                                     {link}
                                 </Link>
-                            </li>
-                        ))
-                    }
-                </ul>
+                            ))
+                        }
+                    </ul>
 
-                {/* btn for lg devices */}
-                <div className='space-x-12 hidden lg:flex items-center'>
-                    <button><FaBarsStaggered className='w-5 hover:text-blue-700' /></button>
-                </div>
+                    {/* btn for lg devices */}
+                    <div className='space-x-12 hidden lg:flex items-center'>
+                        <button><FaBarsStaggered className='w-5 hover:text-blue-700' /></button>
+                    </div>
 
-                {/* menu btn for mobile devices */}
-                <div className='md:hidden'>
-                    <button onClick={toggleMenu} className='text-black focus:outline-none'>
-                        {isMenuOpen ? <FaXmark className='h-5 w-5 text-black' /> : <FaBarsStaggered className='h-5 w-5 text-black' />}
-                    </button>
+                    {/* menu btn for mobile devices */}
+                    <div className='md:hidden'>
+                        <button onClick={toggleMenu} className='text-black focus:outline-none'>
+                            {isMenuOpen ? <FaXmark className='h-5 w-5 text-black' /> : <FaBarsStaggered className='h-5 w-5 text-black' />}
+                        </button>
+                    </div>
                 </div>
             </nav>
 
--- gameNova-client/src/home/Home.jsx
+++ gameNova-client/src/home/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
+import Banner from "../components/Banner";
 const Home = () => {
     return (
-        <div className='h-screen'>
-            <div className='h-screen'>Home
-        </div>
-        <div className='h-screen bg-red-600'></div>
+        <div>
+            <Banner/>
         </div>
     )
 }
