82aec5219c7315f904a50b75bf3298a471f92c69
Shuhrid Abrar <139622785+XGNoir95@users.noreply.github.com>
2024-07-12 02:11:46 +0600

    Update RecentlyAdd.jsx

--- frontend/src/components/Home/RecentlyAdd.jsx
+++ frontend/src/components/Home/RecentlyAdd.jsx
@@ -8,7 +8,9 @@ const RecentlyAdd = () => {
   useEffect(() => {
     const fetch = async () => {
       try {
-        const response = await axios.get("http://localhost:1000/api/v1/get-recent-games");
+        const response = await axios.get("https://gn-old-api.vercel.app/api/v1/get-recent-games", {
+          withCredentials: true
+        });
         console.log(response.data.data);
         setData(response.data.data);
       } catch (error) {
