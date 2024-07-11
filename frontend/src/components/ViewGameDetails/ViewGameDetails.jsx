d51682bc2947e7ddc9fc43da1217ecdfb82c056d
Shuhrid Abrar <139622785+XGNoir95@users.noreply.github.com>
2024-07-12 02:18:38 +0600

    Update ViewGameDetails.jsx

--- frontend/src/components/ViewGameDetails/ViewGameDetails.jsx
+++ frontend/src/components/ViewGameDetails/ViewGameDetails.jsx
@@ -10,7 +10,9 @@ const ViewGameDetails = () => {
     useEffect(() => {
         const fetch = async () => {
             try {
-                const response = await Axios.get(`http://localhost:1000/api/v1/get-game-by-id/${id}`);
+                const response = await Axios.get(`https://gn-old-api.vercel.app/api/v1/get-game-by-id/${id}`, {
+                    withCredentials: true
+                });
                 setData(response.data.data);
             } catch (error) {
                 console.error('Error fetching game data:', error);
@@ -22,7 +24,7 @@ const ViewGameDetails = () => {
 
     return (
         <div>
-            <h1></h1>
+            <h1>Game Details</h1>
             {/* Display game details here */}
             {data && (
                 <div>
